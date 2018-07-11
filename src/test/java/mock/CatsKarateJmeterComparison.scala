35d0643c7267a8048135cb0fa7fbc44b5d26284c
Peter Thomas <peter_thomas@intuit.com>
2018-07-11 15:43:37 +0530

    karate config so that urls aggregate correctly in gatling report

--- src/test/java/mock/CatsKarateJmeterComparison.scala
+++ src/test/java/mock/CatsKarateJmeterComparison.scala
@@ -8,11 +8,16 @@ import scala.concurrent.duration._
 class CatsKarateJmeterComparison extends Simulation {
   MockUtils.startServer()
 
+  val protocol = karateProtocol(
+    "/cats/{id}" -> pauseFor("get" -> 0, "delete" -> 0),
+    "/cats" -> pauseFor("get" -> 0, "post" -> 0)
+  )
+
   val flow = scenario("create").repeat(10) {
     exec(karateFeature("classpath:mock/cats-flow.feature"))
   }
 
   setUp(
-    flow.inject(rampUsers(10) over (5 seconds))
+    flow.inject(rampUsers(10) over (5 seconds)).protocols(protocol)
   )
 }
